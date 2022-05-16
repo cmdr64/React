1816a79486cdc1c1d9b881cba2916442f35f18d8
cory <cory@fake.com>
2022-05-16 16:02:54 -0500

    using fragment now

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Counter extends Component {
     render() { 
         return (
-            <div>
+            <React.Fragment>
                 <h1>it's true</h1>
                 <button>i do not think react is smart</button>
-            </div>
+            </React.Fragment>
         );
     }
 }
